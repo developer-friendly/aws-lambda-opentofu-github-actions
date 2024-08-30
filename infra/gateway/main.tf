1c9d83acc0f7712afa9ec0f79b5f9a1d09bee56e
Meysam Azad <meysam@developer-friendly.blog>
2024-08-30 10:32:49 +0700

    fix: finally made it work by specifying one wildcard in source arn

--- infra/gateway/main.tf
+++ infra/gateway/main.tf
@@ -12,6 +12,6 @@ resource "aws_api_gateway_resource" "this" {
 resource "aws_api_gateway_method" "this" {
   rest_api_id   = aws_api_gateway_rest_api.this.id
   resource_id   = aws_api_gateway_resource.this.id
-  http_method   = "GET"
+  http_method   = "ANY"
   authorization = "NONE"
 }
--- infra/lambda/main.tf
+++ infra/lambda/main.tf
@@ -63,7 +63,7 @@ resource "aws_lambda_permission" "this" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.this.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${var.api_gateway_rest_api_execution_arn}/*/*"
+  source_arn    = "${var.api_gateway_rest_api_execution_arn}/*"
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
